3b88c91b7176fcb003369a037feec312df9afebe
Vinay Naugain <vinay96naugain@gmail.com>
2026-06-09 07:20:11 +0530

    refactor: clean up ClientStatusPieChart component; remove console log and improve formatting

--- frontend/src/components/dashboard/ClientStatusPieChart.jsx
+++ frontend/src/components/dashboard/ClientStatusPieChart.jsx
@@ -9,7 +9,6 @@ import {
 } from "recharts";
 
 function ClientStatusPieChart({ stats }) {
-  console.log(stats)
   const data = [
     { name: "Active", value: stats?.activeClients || 0 },
     { name: "Inactive", value: stats?.inactiveClients || 0 },
@@ -19,25 +18,35 @@ function ClientStatusPieChart({ stats }) {
 
   const COLORS = ["#00C49F", "#FF8042", "#FFBB28", "#FF4D4F"];
   return (
-    <div style={{
-
+    <div
+      style={{
         height: "400px",
 
-        border:
-          "1px solid #ddd",
+        border: "1px solid #ddd",
 
         borderRadius: "8px",
 
         padding: "20px",
-      }}>
+      }}
+    >
       <h3>Client Status Distribution</h3>
       <ResponsiveContainer width="100%" height="90%">
         <PieChart>
-          <Pie data={data} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={120} label >
-            {data.map((entry, index)=> <Cell key={index} fill={COLORS[index % COLORS.length]}/>)}
+          <Pie
+            data={data}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={120}
+            label
+          >
+            {data.map((entry, index) => (
+              <Cell key={index} fill={COLORS[index % COLORS.length]} />
+            ))}
           </Pie>
-          <Tooltip/>
-          <Legend/>
+          <Tooltip />
+          <Legend />
         </PieChart>
       </ResponsiveContainer>
     </div>
