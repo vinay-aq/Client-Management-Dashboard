7920a10ca2d0f6d689938f74c94c1cbb550c5523
Vinay Naugain <vinay96naugain@gmail.com>
2026-08-21 19:05:27 +0530

    style: format code for consistency in activity.route.js

--- backend/src/modules/activity/activity.route.js
+++ backend/src/modules/activity/activity.route.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {authMiddleware} = require("../auth/auth.middleware");
-const {getActivity,fetchActivitiesByEntity} = require("./activity.controller")
+const { authMiddleware } = require("../auth/auth.middleware");
+const {
+  getActivity,
+  fetchActivitiesByEntity,
+} = require("./activity.controller");
 
-router.get('/', authMiddleware, getActivity);
-router.get('/:entityType/:entityId', authMiddleware, fetchActivitiesByEntity);
-
-
+router.get("/", authMiddleware, getActivity);
+router.get("/:entityType/:entityId", authMiddleware, fetchActivitiesByEntity);
 
 module.exports = router;
